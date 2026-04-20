047ab889cc44496108de845a344b1660d91a8fea
d3rr1ck27 <138870853+d3rr1ck27@users.noreply.github.com>
2026-04-20 12:46:45 +0200

    Fix missing imports in ProjectController

--- src/main/java/com/derricklove/controller/ProjectController.java
+++ src/main/java/com/derricklove/controller/ProjectController.java
@@ -3,8 +3,14 @@ package com.derricklove.controller;
 import com.derricklove.model.Project;
 import com.derricklove.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController

e5cf49bdc85898d949f5d657379fd1f9d3d51aa3
d3rr1ck27 <138870853+d3rr1ck27@users.noreply.github.com>
2026-04-20 12:59:31 +0200

    Fix CORS typo in ContactController

--- src/main/java/com/derricklove/controller/ContactController.java
+++ src/main/java/com/derricklove/controller/ContactController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = {"http://localhost:3000", "https://derrick-portfollio-frontend.vercel.app"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://derrick-portfolio-frontend.vercel.app"})
 @RequiredArgsConstructor
 public class ContactController {
 
